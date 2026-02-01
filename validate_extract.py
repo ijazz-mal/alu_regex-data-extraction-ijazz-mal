7df2f6275b2707cbb93324f6c6b3ea52a37947c2
INEZA Henry Jayz <ihenryjay@alustudent.com>
2026-02-01 04:25:43 +0200

    added hashtag and URL patterns

--- validate_extract.py
+++ validate_extract.py
@@ -4,5 +4,13 @@ with open("inputs.txt", "r", encoding="utf-8") as f: #the utf-8 encoding is used
     txt = f.read()
 
 timepattern = r"\b((?:(?:0?[1-9]|1[0-2]):[0-5][0-9]\s?(?:AM|PM|am|pm))|(?:(?:[01][0-9]|2[0-3]):[0-5][0-9]))\b" #time patterns like "23:59" or "11:30 AM"
-
 time_candidates = re.findall(timepattern, txt)
+
+URLpattern = r"\b(https?://[^\s<>#%~`\"]+\s)" #URLs starting with http or https excluding spaces and certain special characters
+URL_candidates = re.findall(URLpattern, txt)
+
+hashtagpattern = r"(#[A-Za-z0-9_]+)" #hashtags like #example_01
+hashtag_candidates = re.findall(hashtagpattern, txt)
+print("Time Candidates:", time_candidates)
+print("URL Candidates:", URL_candidates)
+print("Hashtag Candidates:", hashtag_candidates)
